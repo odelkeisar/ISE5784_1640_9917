298aa80bf8b43addcef7a94853ad2a34b8fce1a7
keisarodel@gmail.com <keisarodel@gmail.com>
2024-05-14 01:11:40 +0300

    מימוש בדיקות ל Triangle ומימוש לבנאי של Triangle

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,8 +1,20 @@
 package geometries;
-
+import primitives.*;
 /**
  * Represents a triangle in 3D space, extending Polygon.
  */
+/**
+ * Represents a triangle in three-dimensional space, extending Polygon.
+ */
 public class Triangle extends Polygon {
 
+    /**
+     * Constructs a new Triangle with the specified vertices.
+     * @param a The first vertex of the triangle.
+     * @param b The second vertex of the triangle.
+     * @param c The third vertex of the triangle.
+     */
+    public Triangle(Point p1, Point p2, Point p3) {
+        super(p1, p2, p3);
+    }
 }
--- unittests/geometries/TriangleTest.java
+++ unittests/geometries/TriangleTest.java
@@ -1,7 +1,39 @@
 package geometries;
 
+import org.junit.jupiter.api.Test;
+import primitives.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the Triangle class.
+ */
 class TriangleTest {
 
+    /**
+     * Test method for {@link geometries.Triangle#Triangle(primitives.Point, primitives.Point, primitives.Point)}.
+     */
+    @Test
+    public void testConstructor() {
+
+        // =============== Boundary Values Tests ==================
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Triangle(new Point(1, 2, 0), new Point(1, 2, 0), new Point(1, 1, 1)),
+                "ERROR: Failed constructing a correct triangle");
+        assertThrows(IllegalArgumentException.class,
+                () -> new Triangle(new Point(2, 4, 0), new Point(1, 2, 0), new Point(3, 6, 0)),
+                "ERROR: Failed constructing a correct triangle");
+    }
+
+    /**
+     * Test method for {@link geometries.Triangle#getNormal(primitives.Point)}.
+     */
+    @Test
+    public void testGetNormal() {
+
+        // ============ Equivalence Partitions Tests ==============
+        Triangle t = new Triangle(new Point(0, 0, 0), new Point(1, 0, 0), new Point(0, 1, 0));
+        assertEquals(new Vector(0, 0, 1), t.getNormal(new Point(0, 0, 0)), "ERROR: GetNormal ERROR");
+    }
 }
